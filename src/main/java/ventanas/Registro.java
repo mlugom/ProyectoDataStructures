5afaa9e96891e079acd21eadea986eb6f148da77
festepaq <36159490+festepaq@users.noreply.github.com>
2019-09-25 22:35:53 -0500

    Update Registro.java

--- src/main/java/ventanas/Registro.java
+++ src/main/java/ventanas/Registro.java
@@ -6,6 +6,7 @@
 package ventanas;
 
 import clases.*;
+import java.awt.Robot;
 import java.io.*;
 import java.util.*;
 import javax.swing.JOptionPane;
@@ -16,7 +17,29 @@ import javax.swing.JOptionPane;
  */
 public class Registro {
 
-    public static void main(String[] args) throws IOException {
+    public static void limpiarPantalla() {
+        try {
+            Robot robbie = new Robot();
+            robbie.keyPress(17);
+            robbie.keyPress(76);
+            robbie.keyRelease(17);
+            robbie.keyRelease(76);
+
+            Thread.sleep(100);
+
+            robbie.keyPress(17);
+            robbie.keyPress(16);
+            robbie.keyPress(82);
+            robbie.keyRelease(17);
+            robbie.keyRelease(16);
+            robbie.keyRelease(82);
+            Thread.sleep(100);
+        } catch (Exception ex) {
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
 
         ArrayList<Usuario> arreglo_empleados = new ArrayList<>();
         ArrayList<Usuario> arreglo_clientes = new ArrayList<>();
@@ -62,62 +85,71 @@ public class Registro {
         }
 
         Scanner sc = new Scanner(System.in);
+        boolean estado = true;
+        do {
+            limpiarPantalla();
+            System.out.println("/////////////////////////////////////////");
+            System.out.println("              Bienvenido");
+            System.out.println("¿Qué desea hacer?");
+            System.out.println("1) Ingresar usuario");
+            System.out.println("2) Registrar usuario");
+            // System.out.println("3) Consultar usuario");
+            System.out.println("4) Consultar pelicula");
+            System.out.println("/////////////////////////////////////////");
 
-        System.out.flush();
-        System.out.println("/////////////////////////////////////////");
-        System.out.println("              Bienvenido");
-        System.out.println("¿Qué desea hacer?");
-        System.out.println("1) Ingresar usuario");
-        System.out.println("2) Registrar usuario");
-        // System.out.println("3) Consultar usuario");
-        System.out.println("4) Consultar pelicula");
-        System.out.println("/////////////////////////////////////////");
+            int seleccion = sc.nextInt();
+            switch (seleccion) {
+                case 1:
+                    Thread.sleep(3000);
+                        limpiarPantalla();
+break;
+                case 2:
+                    limpiarPantalla();
+                    System.out.println("Ingrese nombre");
+                    String nombre_dinamico = sc.next();
+                    System.out.println("Ingrese edad");
+                    int edad_dinamico = sc.nextInt();
+                    System.out.println("Ingrese id");
+                    int id_dinamico = sc.nextInt();
+                    System.out.println("¿Es usted empleado? (si o no)");
+                    String rol_dinamico = sc.next();
 
-        int seleccion = sc.nextInt();
-        switch (seleccion) {
-            case 1:
+                    if (rol_dinamico.equals("si")) {
+                        System.out.println("Ingrese clave (dada por la empresa)");
+                        // La clave por defecto es 0000
+                        String clave_dinamico = sc.next();
+                        if (clave_dinamico.equals("0000")) {
+                            arreglo_empleados.add(new Cliente(nombre_dinamico, edad_dinamico, id_dinamico, true));
+                            System.out.println("Registrado con éxito");
+                            Thread.sleep(3000);
+                        limpiarPantalla();
+                        } else {
+                            System.out.println("Clave incorrecta");
+                            Thread.sleep(3000);
+                        limpiarPantalla();
+                        }
 
-            case 2:
-                for (int i = 0; i < 30; i++) {
-                    System.out.println("");
-                }
-                System.out.println("Ingrese nombre");
-                String nombre_dinamico = sc.next();
-                System.out.println("Ingrese edad");
-                int edad_dinamico = sc.nextInt();
-                System.out.println("Ingrese id");
-                int id_dinamico = sc.nextInt();
-                System.out.println("¿Es usted empleado? (si o no)");
-                String rol_dinamico = sc.next();
-
-                if (rol_dinamico.equals("si")) {
-                    System.out.println("Ingrese clave (dada por la empresa)");
-                    // La clave por defecto es 0000
-                    String clave_dinamico = sc.next();
-                    if (clave_dinamico.equals("0000")) {
-                        arreglo_empleados.add(new Cliente(nombre_dinamico, edad_dinamico, id_dinamico, true));
-                        System.out.println("Registrado con éxito");
                     } else {
-                        System.out.println("Clave incorrecta");
+
+                        wr.write(nombre_dinamico);
+                        wr.write(";" + edad_dinamico + ";" + id_dinamico + ";" + "false");
+                        wr.write('\n');
+
+                        arreglo_clientes.add(new Cliente(nombre_dinamico, edad_dinamico, id_dinamico, false));
+                        System.out.println("Registrado con éxito");
+                        Thread.sleep(3000);
+                        limpiarPantalla();
+
                     }
+                    wr.close();
+                    bw.close();
+                    break;
+                case 3:
+                    Thread.sleep(3000);
+                        limpiarPantalla();
+            }
 
-                } else {
-
-                    wr.write(nombre_dinamico);
-                    wr.write(";" + edad_dinamico + ";" + id_dinamico + ";" + "false");
-                    wr.write('\n');
-
-                    arreglo_clientes.add(new Cliente(nombre_dinamico, edad_dinamico, id_dinamico, false));
-                    System.out.println("Registrado con éxito");
-
-                }
-                wr.close();
-                bw.close();
-        }
-        
-        
-        case 3:
+        } while (estado = true);
 
     }
 }
-
