ef506758fd98999f52fb684dd225130e53636edf
festepaq <36159490+festepaq@users.noreply.github.com>
2019-09-25 22:06:38 -0500

    Update Usuario.java

--- src/main/java/clases/Usuario.java
+++ src/main/java/clases/Usuario.java
@@ -10,9 +10,51 @@ package clases;
  * @author Manuel Esteban
  */
 public class Usuario {
-    String nombre;
-    int edad;
-    int documento;
-    boolean rol;
+    
+    private String nombre;
+    private int edad;
+    private int documento;
+    private boolean rol;
+
+    public Usuario(String nombre, int edad, int documento, boolean rol) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.documento = documento;
+        this.rol = rol;
+    }
+
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(int documento) {
+        this.documento = documento;
+    }
+
+    public boolean isRol() {
+        return rol;
+    }
+
+    public void setRol(boolean rol) {
+        this.rol = rol;
+    }
+    
     
 }
